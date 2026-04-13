460f3214abef58cf7b448848bc6c2fcf69e880c5
Ole Hansen <olis.hansen@gmail.com>
2026-04-13 18:19:51 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const DATA = {
+  employees: [
+    { name: 'Olis Hansen' },
+    { name: 'Kenneth Vallestad' },
+    { name: 'Paul Chilver' },
+    { name: 'Aliaksandr Skliarenka' }
+  ],
+
+  entries: [
+    {
+      employee: 'Olis Hansen',
+      project: 'Onshore',
+      start: '2026-01-01',
+      end: '2026-03-01'
+    },
+    {
+      employee: 'Aliaksandr Skliarenka',
+      project: 'PBF California',
+      start: '2026-01-10',
+      end: '2026-01-20'
+    }
+  ]
+};
