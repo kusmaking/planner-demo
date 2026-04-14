43fe3bade7fc5caf9c4d4e85573c454a8151c6f8
Ole Hansen <olis.hansen@gmail.com>
2026-04-14 12:24:45 +0200

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,17 +1,1 @@
-function renderCalendar() {
-  const el = document.getElementById("calendar");
-  el.innerHTML = "";
 
-  store.entries.forEach(e => {
-    const p = store.projects.find(x => x.id === e.project_id);
-
-    const div = document.createElement("div");
-    div.innerText = `${e.employee} → ${p.name} (${e.start} - ${e.end})`;
-
-    if (p.status === "Avsluttet") {
-      div.style.background = "#ccc";
-    }
-
-    el.appendChild(div);
-  });
-}
